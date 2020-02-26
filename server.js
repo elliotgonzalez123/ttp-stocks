bf250a2cb7bf9ec69c57ef5a0ef1a4a35793fe48
Elliot Gonzalez <elliotgrey@gmail.com>
2020-02-26 12:39:31 -0500

    user model added along with signup route

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('running');
-});
+connectDB();
+app.use(express.json({ extended: false }));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
