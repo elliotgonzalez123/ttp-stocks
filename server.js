9e1458aabcae840f202dbf3b71684d36f85aeb3f
Elliot Gonzalez <elliotgrey@gmail.com>
2020-02-28 13:15:49 -0500

    prep for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
 const cors = require('cors');
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 connectDB();
@@ -13,5 +14,12 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/transactions', require('./routes/api/transactions'));
 app.use('/api/portfolio', require('./routes/api/portfolio'));
 app.use('/api/stock', require('./routes/api/stock'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
